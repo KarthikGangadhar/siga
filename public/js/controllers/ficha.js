acee88d146eefc754d64aed755911c9abb9fa1ae
jaywalking101 <jaywalking101@gmail.com>
2013-11-26 00:05:00 -0300

    Avanços no controller ficha. Refatoração completa do html em ficha.html.
    Acréscimo de validação básica na view home. Aprimoramento dos inputs
    (maxlength, show, hide etc.).

--- public/js/controllers/ficha.js
+++ public/js/controllers/ficha.js
@@ -1,5 +1,37 @@
 angular
     .module('sg')
-    .controller('ficha', function ficha($scope) {
+    .controller('ficha', function ficha($scope, cadastroDePessoaFisica, random, inscricao) {
+
+        'use strict';
+
+        $scope.helpers = {
+
+            cpf: cadastroDePessoaFisica(),
+
+            data_de_nascimento: random.getRandomInt(13, 28).toString(10) + '/' + random.getRandomInt(1, 12).toString(10) + '/' + random.getRandomInt(1948, 1995).toString(10)
+
+        };
+
+        $scope.inscrito = inscricao().inscrito;
+
+        $scope.detalhes = inscricao().detalhes;
+
+        $scope.form_actions = {
+
+            reset: inscricao.reset
+
+        };
+
+        $scope.isInvalidField = function isInvalidField(fieldName) {
+
+            return $scope.ficha[fieldName].$invalid && $scope.ficha[fieldName].$dirty;
+
+        };
+
+        $scope.isRequired = function isRequired(fieldName) {
+
+            return $scope.ficha[fieldName].$invalid && $scope.ficha[fieldName].$pristine;
+
+        };
 
     });
