e12acc811a9baa2e4eb88ad900caaebd05ac4f95
jaywalking101 <jaywalking101@gmail.com>
2013-11-29 19:46:09 -0300

    Sistema de notificação angular-growl com interface local messenger.js
    Arquivos js e arquivo css + messenger.js

---
+++ public/js/services/messenger.js
@@ -0,0 +1,47 @@
+angular
+    .module('sg')
+    .factory('messenger', function messenger(growl) {
+
+        'use strict';
+
+        return {
+
+            warning: function (text, time) {
+
+                growl.addWarnMessage(text, {
+                    enableHtml: true,
+                    ttl: time || 10000
+                });
+
+            },
+
+            info: function (text, time) {
+
+                growl.addInfoMessage(text, {
+                    enableHtml: true,
+                    ttl: time || 10000
+                });
+
+            },
+
+            success: function (text, time) {
+
+                growl.addSuccessMessage(text, {
+                    enableHtml: true,
+                    ttl: time || 10000
+                });
+
+            },
+
+            danger: function (text, time) {
+
+                growl.addErrorMessage(text, {
+                    enableHtml: true,
+                    ttl: time || 10000
+                });
+
+            }
+
+        };
+
+    });
